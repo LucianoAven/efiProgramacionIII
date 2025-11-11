7e3da2aef849b21afa69a3c8f35be0286947249a
LucianoAven <luavendano22@gmail.com>
2025-11-11 02:49:51 -0300

    Actualizacion del archivo db

--- API-REST-DB/db/db.js
+++ API-REST-DB/db/db.js
@@ -1,5 +1,23 @@
-const { Sequelize } = require('sequelize')
+const { Sequelize } = require('sequelize');
 
+// Usar variables de entorno (definidas en Render)
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306, // por si no está definida
+    dialect: 'mysql',
+    logging: false, // opcional, evita mostrar logs SQL
+  }
+);
+
+module.exports = sequelize;
+
+
+/* 
+const { Sequelize } = require('sequelize')
 
 const sequelize = new Sequelize('crud_db', 'root', 'root1234', {
     host: 'localhost',
@@ -7,3 +25,4 @@ const sequelize = new Sequelize('crud_db', 'root', 'root1234', {
 })
 
 module.exports = sequelize
+ */
