0f8e99665ac8e17c5845028008fd804c2fbf5fa9
LucianoAven <luavendano22@gmail.com>
2025-11-11 01:49:00 -0300

    Ajuste de puerto para deploy en Render

--- API-REST-DB/index.js
+++ API-REST-DB/index.js
@@ -4,7 +4,9 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
-const port = 3000
+const PORT = process.env.PORT || 3000;
+/* const port = 3000 */
+
 app.use(cors())
 app.use(express.json())
 
@@ -20,6 +22,12 @@ app.use('/horarios', horariosRouter)
 app.use('/solicitudes-horarios', solicitudesHorariosRouter)
 app.use('/auth', authRouter)
 
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
+
+/*
 app.listen(port, () => {
     console.log(`servidor corriendo en localhost:${port}`)
 })  
+*/
